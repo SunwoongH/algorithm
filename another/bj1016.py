5b2c74892f01010157983bcb622d1a893b529a3d
SunWoong <sunwoong1122@gmail.com>
2021-11-08 22:32:17 +0900

    another

---
+++ another/bj1016.py
@@ -0,0 +1,28 @@
+import sys
+import math
+
+minV, maxV = map(int, sys.stdin.readline().split())
+nums = list([i for i in range(minV, maxV + 1)])
+
+firstV = nums[0]
+lastV = nums[len(nums) - 1]
+for i in range(2, round(math.sqrt(nums[len(nums) - 1])) + 1):
+    start = firstV // (i ** 2)
+    for j in range((i ** 2) * start , lastV + 1, i ** 2):
+        if j - firstV < 0 or nums[j - firstV] == 0: continue
+        else: nums[j - firstV] = 0
+print(len(nums) - nums.count(0))
+
+
+
+
+
+
+
+'''
+for i in range(2, round(math.sqrt(nums[len(nums) - 1])) + 1):
+    for j in range(len(nums)):
+        if result[j] == True:
+            if nums[j] % (i ** 2) == 0: result[j] = False
+print(len(result))
+'''
