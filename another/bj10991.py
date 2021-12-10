daf6697fa0a506fd40c17c2eeb6e1ea71eb4d7f1
SunWoong <sunwoong1122@gmail.com>
2021-12-10 23:50:30 +0900

    another

---
+++ another/bj10991.py
@@ -0,0 +1,7 @@
+n = int(input())
+for i in range(1, n + 1):
+    print(' ' * (n - i), end='')
+    for j in range(1, i * 2):
+        if j % 2 == 0: print(' ', end='')
+        else: print('*', end='')
+    print()
