68605cbfb374782d7e448c374fd10d0e41a5be23
SunWoong <sunwoong1122@gmail.com>
2022-01-31 21:19:53 +0900

    practice

---
+++ Baekjoon/단계별풀이/step14/bj15649.py
@@ -0,0 +1,18 @@
+'''
+문제 - N과 M (1)
+
+풀이 방법 - 재귀 구조 DFS로 풀이
+'''
+import sys
+n, m = map(int, sys.stdin.readline().split())
+path = []
+def dfs():
+    if len(path) == m:
+        print(*path)
+        return
+    for i in range(1, n + 1):
+        if i not in path:
+            path.append(i)
+            dfs()
+            path.pop()
+dfs()
