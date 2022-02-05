d2df19aef5b599bd5eb4b4f08f367e195be3a319
SunWoong <sunwoong1122@gmail.com>
2022-02-05 19:49:00 +0900

    modify algorithm file

--- Baekjoon/bj1927.py
+++ Baekjoon/bj1927.py
@@ -1,52 +1,48 @@
+'''
+문제 - 최소 힙
+
+풀이 방법 - 재귀적으로 힙 특성을 재구축하는 함수를 구현하여 최소 힙 설계
+'''
 import sys
 
 class Heap:
     def __init__(self):
-        self.heap = [0]
-        self.size = 0
-    
-    def isEmpty(self):
-        return self.size == 0
-
-    def insertMinHeap(self, key):
-        self.heap.append(key)
-        self.size += 1
-        i = self.size
-
-        while i != 1 and key < self.heap[i // 2]:
-            self.heap[i] = self.heap[i // 2]
-            i //= 2
-        self.heap[i] = key
-
-    def deleteMinHeap(self):
-        if self.isEmpty():
-            return False
-        removed = self.heap[1]
-        tempKey = self.heap[self.size]
-        self.size -= 1
-        parent = 1
-        child = 2
-
-        while child <= self.size:
-            if (child < self.size) and (self.heap[child] > self.heap[child + 1]):
+        self.__items = []
+        
+    def __percolate_up(self, i: int) -> None:
+        parent = (i - 1) // 2
+        if i > 0 and self.__items[i] < self.__items[parent]:
+            self.__items[i], self.__items[parent] = self.__items[parent], self.__items[i]
+            self.__percolate_up(parent)
+        
+    def insert(self, item) -> None:
+        self.__items.append(item)
+        self.__percolate_up(len(self.__items) - 1)
+        
+    def __percolate_down(self, i: int) -> None:
+        child = i * 2 + 1
+        if child <= len(self.__items) - 1:
+            if child < len(self.__items) - 1 and self.__items[child] > self.__items[child + 1]:
                 child += 1
-            if tempKey <= self.heap[child]: break
-            self.heap[parent] = self.heap[child]
-            parent = child
-            child *= 2
-        self.heap[parent] = tempKey
+            if self.__items[i] > self.__items[child]:
+                self.__items[i], self.__items[child] = self.__items[child], self.__items[i]
+                self.__percolate_down(child)
+        
+    def delete(self):
+        if self.is_empty():
+            return 0
+        removed = self.__items[0]
+        self.__items[0] = self.__items[len(self.__items) - 1]
+        self.__items.pop()
+        self.__percolate_down(0)
         return removed
+    
+    def is_empty(self) -> bool:
+        return len(self.__items) == 0
 
+n = int(sys.stdin.readline())
 heap = Heap()
-N = int(sys.stdin.readline())
-for i in range(0, N):
-    num = int(sys.stdin.readline())
-    if num == 0:
-        minV = heap.deleteMinHeap()
-        if minV == False:
-            print(0)
-        else:
-            print(minV)
-    else:
-        heap.insertMinHeap(num)
-
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0: print(heap.delete())
+    else: heap.insert(x)
