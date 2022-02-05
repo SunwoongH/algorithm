18f1c1a831c666e9fc864af6d685a40a5c6fd38f
SunWoong <sunwoong1122@gmail.com>
2022-02-05 19:49:46 +0900

    modify algorithm file

--- Baekjoon/bj10866.py
+++ Baekjoon/bj10866.py
@@ -1,61 +1,86 @@
+'''
+문제 - 덱
+
+풀이 방법 - front, rear 더미 노드 포인터를 활용한 단순 연결 리스트로 구현
+'''
 import sys
 
+class DequeNode:
+    def __init__(self, item=None, prev=None, next=None):
+        self.item = item
+        self.prev = prev
+        self.next = next
+        
 class Deque:
-    def __init__(self, n):
-        self.size = n + 1
-        self.data = [0] * self.size
-        self.front = 0
-        self.rear = 0
-    def isEmpty(self):
-        if self.front == self.rear: return 1
-        else: return 0
-    def pushFront(self, key):
-        self.data[self.front] = key
-        self.front = (self.front - 1 + self.size) % self.size
-    def pushBack(self, key):
-        self.rear = (self.rear + 1) % self.size
-        self.data[self.rear] = key
-    def popFront(self):
-        if self.isEmpty(): return -1
-        self.front = (self.front + 1) % self.size
-        return self.data[self.front]
-    def popBack(self):
-        if self.isEmpty(): return -1
-        removed = self.data[self.rear]
-        self.rear = (self.rear - 1 + self.size) % self.size
-        return removed
-    def peekFront(self):
-        if self.isEmpty(): return -1
-        return self.data[(self.front + 1) % self.size]
-    def peekBack(self):
-        if self.isEmpty(): return -1
-        return self.data[self.rear]
-    def sizeD(self):
-        if self.isEmpty(): return 0
-        temp = self.rear
-        count = 0
-        while temp != self.front:
-            count += 1
-            temp = (temp - 1 + self.size) % self.size
-        return count
-
+    def __init__(self):
+        self.__front = DequeNode('dummy')
+        self.__rear = DequeNode('dummy')
+        self.__front.next, self.__rear.prev = self.__rear, self.__front
+        self.__size = 0
+        
+    def append_left(self, item: int) -> None:
+        new_node = DequeNode(item, self.__front, self.__front.next)
+        new_node.next.prev = new_node
+        self.__front.next = new_node
+        self.__size += 1
+        
+    def append(self, item: int) -> None:
+        new_node = DequeNode(item, self.__rear.prev, self.__rear)
+        new_node.prev.next = new_node
+        self.__rear.prev = new_node
+        self.__size += 1
+        
+    def pop_left(self) -> int:
+        if self.is_empty() == 1:
+            return -1
+        removed = self.__front.next
+        removed_item = removed.item
+        self.__front.next = removed.next
+        removed.next.prev = self.__front
+        self.__size -= 1
+        return removed_item
+        
+    def pop(self) -> int:
+        if self.is_empty() == 1:
+            return -1
+        removed = self.__rear.prev
+        removed_item = removed.item
+        self.__rear.prev = removed.prev
+        removed.prev.next = self.__rear
+        self.__size -= 1
+        return removed_item
+    
+    def peek_front(self) -> int:
+        if self.is_empty() == 1:
+            return -1
+        return self.__front.next.item
+    
+    def peek_rear(self) -> int:
+        if self.is_empty() == 1:
+            return -1
+        return self.__rear.prev.item
+    
+    def is_empty(self) -> int:
+        return 1 if self.__size == 0 else 0
+    
+    def size(self) -> int:
+        return self.__size
+    
 n = int(sys.stdin.readline())
-deque = Deque(n)
-for i in range(n):
-    order = sys.stdin.readline().strip('\n')
-    if order[:9] == 'push_back':
-        deque.pushBack(int(order[9:]))
-    elif order[:10] == 'push_front':
-        deque.pushFront(int(order[10:]))
-    elif order == 'pop_front':
-        print(deque.popFront())
-    elif order == 'pop_back':
-        print(deque.popBack())
-    elif order == 'size':
-        print(deque.sizeD())
-    elif order == 'empty':
-        print(deque.isEmpty())
-    elif order == 'front':
-        print(deque.peekFront())
-    elif order == 'back':
-        print(deque.peekBack())
+deque = Deque()
+order = {'size': deque.size, 'empty': deque.is_empty, \
+         'front': deque.peek_front, 'back': deque.peek_rear}
+for _ in range(n):
+    input_order = sys.stdin.readline().strip('\n')
+    if input_order[:4] == 'push':
+        if input_order[5:10] == 'front':
+            deque.append_left(int(input_order[11:]))
+        else:
+            deque.append(int(input_order[10:]))
+    elif input_order[:3] == 'pop':
+        if input_order[4:] == 'front':
+            print(deque.pop_left())
+        else:
+            print(deque.pop())
+    else:
+        print(order[input_order]())
