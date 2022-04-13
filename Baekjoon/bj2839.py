3f871cd1fd9dba6e73fb7f851972808435cc6177
SunWoong <sunwoong1122@gmail.com>
2022-04-13 22:10:57 +0900

    Baekjoon: 설탕 배달

---
+++ Baekjoon/bj2839.py
@@ -0,0 +1,17 @@
+'''
+Created by sunwoong on 2022/04/13
+'''
+import sys
+
+n = int(sys.stdin.readline())
+dp = [-1 for _ in range(5001)]
+dp[3] = dp[5] = 1
+
+for i in range(6, n + 1):
+    if i % 5 == 0:
+        dp[i] = dp[i - 5] + 1
+    elif i % 3 == 0:
+        dp[i] = dp[i - 3] + 1
+    elif dp[i - 3] > 0 and dp[i - 5] > 0:
+        dp[i] = min(dp[i - 3], dp[i - 5]) + 1
+print(dp[n])
