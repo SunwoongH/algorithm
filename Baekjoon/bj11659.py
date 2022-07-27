8571f0f54bb21b2f1ca8b814b37694677e143417
SunWoong <sunwoong1122@gmail.com>
2022-07-27 18:58:37 +0900

    Baekjoon: 구간 합 구하기 4

---
+++ Baekjoon/bj11659.py
@@ -0,0 +1,12 @@
+'''
+Created by sunwoong on 2022/07/27
+'''
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+nums = [0] + list(map(int, sys.stdin.readline().split()))
+for i in range(1, n + 1):
+    nums[i] += nums[i - 1]
+for _ in range(m):
+    i, j = map(int, sys.stdin.readline().split())
+    print(nums[j] - nums[i - 1])
