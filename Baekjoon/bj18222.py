8486777446b3057701cb63a626d52e98d64089ce
SunWoong <sunwoong1122@gmail.com>
2022-08-25 23:26:41 +0900

    Baekjoon: 투에-모스 문자열

---
+++ Baekjoon/bj18222.py
@@ -0,0 +1,16 @@
+'''
+Created by sunwoong on 2022/08/25
+'''
+import sys
+from math import log2
+
+k = int(sys.stdin.readline())
+string = '00110100110010110'
+count = 0
+while k > 16:
+    count += 1
+    if log2(k).is_integer():
+        k = k // 2
+    else:
+        k -= 2 ** int(log2(k))
+print(string[k]) if count % 2 == 0 else print(0) if string[k] == '1' else print(1)
