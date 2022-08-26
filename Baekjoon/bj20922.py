5f4abcd7245f7c7a47817b672d3c52079666070c
SunWoong <sunwoong1122@gmail.com>
2022-08-26 14:28:25 +0900

    Baekjoon: 겹치는 건 싫어

---
+++ Baekjoon/bj20922.py
@@ -0,0 +1,19 @@
+'''
+Created by sunwoong on 2022/08/26
+'''
+import sys
+import collections
+
+n, k = map(int, sys.stdin.readline().split())
+sequence = list(map(int, sys.stdin.readline().split()))
+count = collections.defaultdict(int)
+result = 0
+left = 0
+for right in range(n):
+    target = sequence[right]
+    count[target] += 1
+    while count[target] > k:
+        count[sequence[left]] -= 1
+        left += 1
+    result = max(result, right - left + 1)
+print(result)
