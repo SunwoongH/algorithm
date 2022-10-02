0405d19f420ccc2155fb80c6257dc1a45a68e362
SunWoong <sunwoong1122@gmail.com>
2022-10-02 19:10:41 +0900

    Baekjoon: 알바생 강호

---
+++ Baekjoon/bj1758.py
@@ -0,0 +1,18 @@
+'''
+Created by sunwoong on 2022/10/02
+'''
+import sys
+
+n = int(sys.stdin.readline())
+people = []
+for _ in range(n):
+    person = int(sys.stdin.readline())
+    people.append(person)
+people.sort(reverse=True)
+
+result = 0
+for i in range(len(people)):
+    tip = people[i] - (i + 1 - 1)
+    if tip > 0:
+        result += tip
+print(result)
