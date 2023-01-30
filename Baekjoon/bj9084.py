f87e469d029febaa85330d2655e9164e04f30457
sunwoong <sunwoong1122@gmail.com>
2023-01-31 01:01:15 +0900

    Baekjoon: 동전 (코드 수정)

--- Baekjoon/bj9084.py
+++ Baekjoon/bj9084.py
@@ -9,10 +9,9 @@ for _ in range(t):
     n = int(input())
     coins = list(map(int, input().split()))
     m = int(input())
-dp = [0 for _ in range(m + 1)]
-dp[0] = 1
-for coin in coins:
-    for i in range(m + 1):
-        if i >= coin:
+    dp = [0 for _ in range(m + 1)]
+    dp[0] = 1
+    for coin in coins:
+        for i in range(coin, m + 1):
             dp[i] += dp[i - coin]
-print(dp[m])
+    print(dp[m])
