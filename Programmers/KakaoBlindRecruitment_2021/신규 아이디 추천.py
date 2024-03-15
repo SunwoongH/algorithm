71a713e77090c3cd56a5f0f4585c459f37962117
sunwoong <sunwoong1122@gmail.com>
2024-03-15 16:54:48 +0900

    Kakao: 신규 아이디 추천 (re)

--- Programmers/KakaoBlindRecruitment_2021/신규 아이디 추천.py
+++ Programmers/KakaoBlindRecruitment_2021/신규 아이디 추천.py
@@ -1,39 +1,59 @@
 '''
-Created by sunwoong on 2022/09/16
+Created by sunwoong on 2024/03/15
 
-풀이 시간 - 33분
+풀이 시간 - 70분
 '''
 
 def solution(new_id):
-    is_possible = {'-', '_', '.'}
-    step_two = ""
-    for char in new_id.lower():
-        if char.isalnum() or char in is_possible:
-            step_two += char
-    step_three = ""
-    double_check = 0
-    for char in step_two:
-        if char == '.':
-            double_check += 1
+    patterns = ['-', '_', '.']
+    answer = list(new_id)
+    #1단계
+    for i in range(len(answer)):
+        if answer[i].isupper():
+            answer[i] = answer[i].lower()
+    #2단계
+    new_answer = []
+    for char in answer:
+        if not char.isalpha() and not char.isdigit() and char not in patterns:
+            continue
+        new_answer.append(char)
+    answer = new_answer
+    #3단계
+    flag = False
+    new_answer = []
+    for char in answer:
+        if not flag:
+            if char == '.':
+                flag = True
+                new_answer.append('.')
+                continue
+            new_answer.append(char)
         else:
-            if double_check > 0:
-                step_three += '.'
-                double_check = 0
-            step_three += char
-    start, end = 0, len(step_three)
-    if len(step_three) > 0 and step_three[0] == '.':
-        start += 1
-    if len(step_three) > 1 and step_three[end - 1] == '.':
-        end -=1
-    step_after_four = step_three[start:end]
-    if not step_after_four:
-        step_after_four = "a"
-    if len(step_after_four) >= 16:
-        step_after_four = step_after_four[:15] if step_after_four[14] != '.' \
-                            else step_after_four[:14]
-    else:
-        if len(step_after_four) <= 2:
-            last_char = step_after_four[-1]
-            while len(step_after_four) < 3:
-                step_after_four += last_char
-    return step_after_four
+            if char != '.':
+                new_answer.append(char)
+                flag = False
+    answer = new_answer
+    #4단계
+    if len(answer) > 1:
+        if answer[0] == '.' and answer[-1] == '.':
+            answer = answer[1:len(answer) - 1]
+        else:
+            if answer[0] == '.':
+                answer = answer[1:len(answer)]
+            elif answer[-1] == '.':
+                answer = answer[:len(answer) - 1]
+    elif len(answer) == 1 and answer[0] == '.':
+        answer = []
+    #5단계
+    if len(answer) == 0:
+        answer.append('a')
+    #6단계
+    if len(answer) > 15:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer.pop()
+    #7단계
+    if len(answer) <= 2:
+        char = answer[-1]
+        answer.extend([char for _ in range(3 - len(answer))])
+    return ''.join(answer)
