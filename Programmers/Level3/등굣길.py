6f5f472ec18ff9b2b4f7cc183acab00abc8614c5
sunwoong <sunwoong1122@gmail.com>
2024-03-19 21:40:04 +0900

    Programmers: 등굣길 (re)

--- Programmers/Level3/등굣길.py
+++ Programmers/Level3/등굣길.py
@@ -1,22 +1,23 @@
 '''
-Created by sunwoong on 2022/12/01
+Created by sunwoong on 2024/03/19
 '''
+move = [(0, 1), (1, 0)]
+
+def dfs(r, c, dp, puddles, m, n):
+    if r == n and c == m:
+        dp[r][c] = 1
+        return dp[r][c]
+    for oper in move:
+        dr = r + oper[0]
+        dc = c + oper[1]
+        if 0 <= dr <= n and 0 <= dc <= m:
+            if [dc, dr] not in puddles:
+                if not dp[dr][dc]:
+                    dp[r][c] += dfs(dr, dc, dp, puddles, m, n)
+                else:
+                    dp[r][c] += dp[dr][dc] 
+    return dp[r][c] % 1_000_000_007
 
 def solution(m, n, puddles):
-    dp = [[0 for _ in range(m)] for _ in range(n)]
-    dp[0][0] = 1
-    water = set()
-    move = [(-1, 0), (0, -1)]
-    for puddle in puddles:
-        water.add(tuple(puddle))
-    for r in range(n):
-        for c in range(m):
-            if (c + 1, r + 1) in water:
-                continue
-            for oper in move:
-                dr = r + oper[0]
-                dc = c + oper[1]
-                if 0 <= dr < n and 0 <= dc < m:
-                    dp[r][c] += dp[dr][dc]
-            dp[r][c] %= 1000000007
-    return dp[n - 1][m - 1]
+    dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+    return dfs(1, 1, dp, puddles, m, n)
