0d5b886f24cc088d124be436d8349c28ad72778a
sunwoong <sunwoong1122@gmail.com>
2024-03-29 16:45:08 +0900

    Programmers: 소수 찾기 (re)

--- Programmers/Level2/소수 찾기.py
+++ Programmers/Level2/소수 찾기.py
@@ -1,25 +1,23 @@
 '''
-Created by sunwoong on 2023/02/28
+Created by sunwoong on 2024/03/29
+
+풀이 시간 - 27분
 '''
 from itertools import permutations
 
 def solution(numbers):
-    SIZE = 10_000_000
-    prime_numbers = [True for _ in range(SIZE)]
-    prime_numbers[0] = prime_numbers[1] = False
-    for i in range(2, int(SIZE ** 0.5) + 1):
-        if prime_numbers[i]:
-            j = 2
-            while i * j < SIZE:
-                prime_numbers[i * j] = False
-                j += 1
-    numbers = list(numbers)
     seen = set()
-    answer = 0
-    for count in range(1, len(numbers) + 1):
-        for case in permutations(numbers, count):
-            number = int(''.join(case))
-            if prime_numbers[number] and number not in seen:
-                answer += 1
-                seen.add(number)
-    return answer
+    numbers = sorted(list(numbers), reverse=True)
+    prime_numbers = [True for _ in range(int(''.join(numbers)) + 1)]
+    prime_numbers[0] = prime_numbers[1] = False
+    for i in range(2, int(int(''.join(numbers)) ** 0.5) + 1):
+        if prime_numbers[i]:
+            for j in range(i * 2, int(''.join(numbers)) + 1, i):
+                if prime_numbers[j]:
+                    prime_numbers[j] = False
+    for i in range(1, len(numbers) + 1):
+        for case in permutations(numbers, i):
+            case = int(''.join(case))
+            if case not in seen and prime_numbers[case]:
+                seen.add(case)
+    return len(seen)
