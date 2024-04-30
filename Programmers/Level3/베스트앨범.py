208dd21757512edd307d6497234ad60dc734dea1
sunwoong <sunwoong1122@gmail.com>
2024-04-30 16:23:24 +0900

    Programmers: 베스트앨범 (re)

--- Programmers/Level3/베스트앨범.py
+++ Programmers/Level3/베스트앨범.py
@@ -1,17 +1,23 @@
 '''
-Created by sunwoong on 2022/10/09
+Created by sunwoong on 2024/04/30
+
+풀이 시간 - 24분
 '''
 from collections import defaultdict
 
 def solution(genres, plays):
-    genres_table = defaultdict(list)
-    for i, genre in enumerate(genres):
-        genres_table[genre].append(i)
-    play_table = defaultdict(int)
-    for i, play in enumerate(plays):
-        play_table[i] += play
-    genres = sorted(genres_table.keys(), key=lambda x: -sum(map(lambda y: play_table[y], genres_table[x])))
     answer = []
-    for genre in genres:
-        answer.extend(sorted(genres_table[genre], key=lambda x: -play_table[x])[:2])
+    genre_table = defaultdict(list)
+    genre_sum = defaultdict(int)
+    for i in range(len(genres)):
+        genre_table[genres[i]].append(i)
+        genre_sum[genres[i]] += plays[i]
+    genre_keys = list(genre_table.keys())
+    genre_keys.sort(key=lambda x: -genre_sum[x])
+    for genre in genre_keys:
+        if len(genre_table[genre]) == 1:
+            answer.append(genre_table[genre][0])
+            continue
+        genre_table[genre].sort(key=lambda x: -plays[x])
+        answer.extend(genre_table[genre][:2])
     return answer
