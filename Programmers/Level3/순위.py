7ad0d4851f4b832ccb952d3024d4521f0ad95377
sunwoong <sunwoong1122@gmail.com>
2024-05-09 16:55:46 +0900

    Programmers: 순위 (re)

--- Programmers/Level3/순위.py
+++ Programmers/Level3/순위.py
@@ -1,33 +1,89 @@
 '''
-Created by sunwoong on 2022/12/25
-'''
-WIN = 1
-LOSE = -1
-UNKNOWN = 0
+Created by sunwoong on 2024/05/09
 
-def update(n, graph):
-    for k in range(n):
-        for r in range(n):
-            for c in range(n):
-                if graph[r][c] == UNKNOWN:
-                    choice = graph[r][k] + graph[k][c]
-                    if choice == 2:
-                        graph[r][c] = WIN
-                    elif choice == -2:
-                        graph[r][c] = LOSE
-               
+풀이 시간 - 50분
+'''
+from collections import defaultdict, deque
+
+def bfs(n, start, graph, visited):
+    visited[start] = True
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                queue.append(next_node)
+
+def is_promising(n, start, graph, end):
+    visited = [False for _ in range(n + 1)]
+    visited[start] = True
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        if node == end:
+            return True
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                queue.append(next_node)
+    return False
+
 def solution(n, results):
-    graph = [[UNKNOWN for _ in range(n)] for _ in range(n)]
-    for r, c in results:
-        graph[r - 1][c - 1] = WIN
-        graph[c - 1][r - 1] = LOSE
-    update(n, graph)
+    graph = defaultdict(list)
+    for winner, loser in results:
+        graph[loser].append(winner)
     answer = 0
-    for c in range(n):
-        count = 0
-        for r in range(n):
-            if graph[r][c] == UNKNOWN:
-                count += 1
-        if count == 1:
+    for i in range(1, n + 1):
+        visited = [False for _ in range(n + 1)]
+        bfs(n, i, graph, visited)
+        available = True
+        for j in range(1, n + 1):
+            if not visited[j] and j != i:
+                if not is_promising(n, j, graph, i):
+                    available = False
+                    break
+        if available:
+            answer += 1
+    return answer
+
+def solution(n, results):
+    answer = 0
+    win, lose = defaultdict(set), defaultdict(set)
+    for winner, loser in results:
+        win[loser].add(winner)
+        lose[winner].add(loser)
+    for player in range(1, n + 1):
+        for winner in win[player]:
+            lose[winner].update(lose[player])
+        for loser in lose[player]:
+            win[loser].update(win[player])
+    for player in range(1, n + 1):
+        if len(win[player]) + len(lose[player]) == n - 1:
+            answer += 1
+    return answer
+
+def solution(n, results):
+    answer = 0
+    graph = [[None for _ in range(n + 1)] for _ in range(n + 1)]
+    for winner, loser in results:
+        graph[winner][loser] = "win"
+        graph[loser][winner] = "lose"
+    for i in range(1, n + 1):
+        graph[i][i] = "self"
+    for k in range(1, n + 1):
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if graph[i][k] == "win" and graph[k][j] == "win":
+                    graph[i][j] = "win"
+                elif graph[i][k] == "lose" and graph[k][j] == "lose":
+                    graph[i][j] = "lose"
+    for i in range(1, n + 1):
+        is_promising = True
+        for j in range(1, n + 1):
+            if not graph[i][j]:
+                is_promising = False
+                break
+        if is_promising:
             answer += 1
     return answer
