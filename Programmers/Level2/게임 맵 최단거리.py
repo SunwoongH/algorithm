62345077f5282532e25011b02951b4d5ed98dcc2
sunwoong <sunwoong1122@gmail.com>
2024-06-19 22:21:01 +0900

    Programmers: 게임 맵 최단거리 (re)

--- Programmers/Level2/게임 맵 최단거리.py
+++ Programmers/Level2/게임 맵 최단거리.py
@@ -1,23 +1,28 @@
 '''
-Created by sunwoong on 2022/09/27
+Created by sunwoong on 2024/06/19
+
+풀이 시간 - 20분
 '''
 from collections import deque
 
-def bfs(r, c, maps):
-    move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-    queue = deque([(r, c, 1)])
-    maps[r][c] = 0
+move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+def bfs(maps):
+    visited = [[False for _ in range(len(maps[0]))] for _ in range(len(maps))]
+    queue = deque([(0, 0, 1)])
+    visited[0][0] = True
     while queue:
-        r, c, distance = queue.popleft()
+        r, c, count = queue.popleft()
         if r == len(maps) - 1 and c == len(maps[0]) - 1:
-            return distance
+            return count
         for oper in move:
             dr = r + oper[0]
             dc = c + oper[1]
-            if 0 <= dr < len(maps) and 0 <= dc < len(maps[0]) and maps[dr][dc]:
-                maps[dr][dc] = 0
-                queue.append((dr, dc, distance + 1))
+            if 0 <= dr < len(maps) and 0 <= dc < len(maps[0]):
+                if maps[dr][dc] and not visited[dr][dc]:
+                    visited[dr][dc] = True
+                    queue.append((dr, dc, count + 1))
     return -1
 
 def solution(maps):
-    return bfs(0, 0, maps)
+    return bfs(maps)
