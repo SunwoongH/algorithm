b98904caa46db4ba13413088c27e918e96bb199a
sunwoong <sunwoong1122@gmail.com>
2024-06-24 15:44:38 +0900

    Programmers: 큰 수 만들기 (re)

--- Programmers/Level2/큰 수 만들기.py
+++ Programmers/Level2/큰 수 만들기.py
@@ -1,45 +1,14 @@
 '''
-Created by sunwoong on 2023/03/07
+Created by sunwoong on 2024/06/24
+
+풀이 시간 - 20분
 '''
-from collections import deque
 
-def solution1(number, k):
-    number = deque(number)
-    seen = []
-    count = 0
-    length = len(number)
-    while count < length and k != 0:
-        if length - count == k:
-            if seen and int(seen[-1]) < int(number[0]):
-                number.appendleft(seen.pop())
-                count -= 1
-            number.popleft()
-            count += 1
+def solution(number, k):
+    temp = []
+    for num in number:
+        while temp and temp[-1] < num and k > 0:
             k -= 1
-            continue
-        if int(number[0]) < int(number[1]):
-            a = number.popleft()
-            if seen and int(seen[-1]) < int(number[0]):
-                number.appendleft(seen.pop())
-                count -= 1
-            count += 1
-            k -= 1
-        else:
-            seen.append(number.popleft())
-            count += 1
-    answer = seen + list(number)
-    idx = len(answer) - 1
-    for i in range(len(answer)):
-        if answer[i] != "0":
-            idx = i
-            break
-    return ''.join(answer[idx:])
-
-def solution2(number, k):
-    answer = [number[0]]
-    for i in range(1, len(number)):
-        while k > 0 and answer and int(answer[-1]) < int(number[i]):
-            answer.pop()
-            k -= 1
-        answer.append(number[i])
-    return ''.join(answer[:len(answer) - k])
+            temp.pop()
+        temp.append(num)
+    return ''.join(temp[:len(temp) - k])
