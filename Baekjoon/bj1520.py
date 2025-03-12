feeda669ecfbe078423b9a9089cedbca93aed1c1
sunwoong <sunwoong1122@gmail.com>
2025-03-12 17:54:32 +0900

    내리막 길

--- Baekjoon/bj1520.py
+++ Baekjoon/bj1520.py
@@ -1,27 +1,29 @@
 '''
-Created by sunwoong on 2023/03/17
+Created by sunwoong on 2025/03/12
 '''
 import sys
-input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
 
-def dfs(r, c):
-    visited[r][c] = True
+m, n = map(int, sys.stdin.readline().split())
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+counting = [[-1 for _ in range(n)] for _ in range(m)]
+
+def dfs(board, r, c):
     if r == m - 1 and c == n - 1:
-        count[r][c] = 1
-        return count[r][c]
+        counting[r][c] = 1
+        return counting[r][c]
+    counting[r][c] = 0
     for oper in move:
         dr = r + oper[0]
         dc = c + oper[1]
-        if 0 <= dr < m and 0 <= dc < n and board[dr][dc] < board[r][c]:
-            if not visited[dr][dc]:
-                count[r][c] += dfs(dr, dc)
-            else:
-                count[r][c] += count[dr][dc]
-    return count[r][c]
+        if 0 <= dr < m and 0 <= dc < n:
+            if board[dr][dc] < board[r][c]:
+                if counting[dr][dc] == -1:
+                    counting[r][c] += dfs(board, dr, dc)
+                else:
+                    if counting[dr][dc] > 0:
+                        counting[r][c] += counting[dr][dc]
+    return counting[r][c]
 
-m, n = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(m)]
-visited = [[False for _ in range(n)] for _ in range(m)]
-count = [[0 for _ in range(n)] for _ in range(m)]
-move = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-print(dfs(0, 0))
+print(dfs(board, 0, 0))
