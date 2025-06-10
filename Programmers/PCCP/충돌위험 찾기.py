18c82eba26498a51cdccc006eb14de99d7a826a3
sunwoong <sunwoong1122@gmail.com>
2025-06-10 23:47:38 +0900

    Programmers: 충돌위험 찾기(re)

--- Programmers/PCCP/충돌위험 찾기.py
+++ Programmers/PCCP/충돌위험 찾기.py
@@ -42,3 +42,52 @@ def solution(points, routes):
             
     return answer
     
+from collections import Counter
+
+def solution(points, routes):
+    record = [[[] for _ in range(101)] for _ in range(101)]
+    table = dict()
+    
+    for i in range(len(points)):
+        table[i + 1] = points[i]
+
+    for i in range(len(routes)):
+        sequence = 1
+        for j in range(len(routes[i]) - 1):
+            route = routes[i]
+            sr, sc = table[route[j]]
+            er, ec = table[route[j + 1]]
+            
+            if j == 0:
+                record[sr][sc].append(sequence)
+            while sr != er or sc != ec:
+                sequence += 1
+                if sr != er:
+                    if sr < er:
+                        sr += 1
+                        record[sr][sc].append(sequence)
+                    else:
+                        sr -= 1
+                        record[sr][sc].append(sequence)
+                    continue
+                if sc != ec:
+                    if sc < ec:
+                        sc += 1
+                        record[sr][sc].append(sequence)
+                    else:
+                        sc -= 1
+                        record[sr][sc].append(sequence)
+    
+    count = 0
+    for r in range(len(record)):
+        for c in range(len(record[0])):
+            time = record[r][c]
+            if not time:
+                continue
+            time = Counter(time)
+            counting = time.most_common()
+            for item in counting:
+                if item[1] > 1:
+                    count += 1
+
+    return count
